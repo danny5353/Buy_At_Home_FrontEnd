5df3810c735ad38af8d3cff5c36ecc6dde5fcb68
danny <dannyzambranasanabria@gmail.com>
2021-03-24 03:02:40 -0400

    Creando Pantalla de inicio para el cliente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ClienteComponent } from './home/cliente/cliente.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    component: ClienteComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
