25b19f74e22733c39cd3149d115bbff44eeceaf8
Ro-Nel <rodrigo.saravia@ucb.edu.bo>
2021-04-02 23:54:11 -0400

    Modelado del Modal

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -9,28 +9,35 @@ export class ModalComponent implements OnInit {
 
   constructor(private modal:NgbModal) { }
   
-
   ngOnInit(): void {
   }
+
 openSM(contenido: any){
   this.modal.open(contenido,{size:'sm'});
 }
+
 openLG(contenido: any){
   this.modal.open(contenido,{size:'lg'});
 }
+
 openXL(contenido: any){
   this.modal.open(contenido,{size:'xl'});
 }
+
 openCentrado(contenido: any){
   this.modal.open(contenido,{centered:true});
 }
+
 openScroll(contenido: any){
   this.modal.open(contenido,{scrollable:true});
 }
+
 openBackground(contenido: any){
   this.modal.open(contenido,{backdropClass:'azul'});
 }
+
 openWindow(contenido: any){
   this.modal.open(contenido,{windowClass:'oscuro'});
 }
+
 }
