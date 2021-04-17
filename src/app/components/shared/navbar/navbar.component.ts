9619cb57a471130468dd69d5b3124216756fe23d
Deny Michel <dennys.michel@ucb.edu.bo>
2021-04-16 21:17:49 -0400

    Iniciando Sprint 3

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+//import { TokenService } from './../../../services/token.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
+/*
+  isLogged = false;
 
+  constructor(private tokenService: TokenService) { }
+
+  ngOnInit() {
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+  }
+
+  onLogOut(): void {
+    this.tokenService.logOut();
+    window.location.reload();
+  }*/
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
