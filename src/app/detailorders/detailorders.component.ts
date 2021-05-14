9b759e0f382a70b166c1d82c59ebfda46dc50e8a
fabriariex-22 <fabricio.crespo.requena@gmail.com>
2021-05-13 22:50:57 -0400

    Agregando rutas

--- src/app/detailorders/detailorders.component.ts
+++ src/app/detailorders/detailorders.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
+/*
 @Component({
-  selector: 'app-detailorders',
-  templateUrl: './detailorders.component.html',
-  styleUrls: ['./detailorders.component.css']
+  selector: 'app-order-detail',
+  templateUrl: './order-detail.component.html',
+  styleUrls: ['./order-detail.component.css']
 })
-export class DetailordersComponent implements OnInit {
+export class OrderDetailComponent implements OnInit {
 
-  constructor() { }
+  order: any[] = [];
+  orde: any[] = [];
+  user: any[] = [];
+  orderId: any[] = [];
+
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
+
+    this.activatedRoute.params.subscribe(params => {
+      this.user = (params['userId'])
+      this.orderId = (params['orderId'])
+      console.log(params['userId']);
+      console.log(params['orderId']);
+    })
+
+    this.http.get('http://localhost:8080/users/' + this.user + '/orders/' + this.orderId)
+    .subscribe((data: any) => {
+      this.order = data;
+      this.orde[0] = data[0];
+      console.log(this.order);
+      console.log(this.orde);
+      }
+    )
+
+   }
 
   ngOnInit(): void {
   }
 
 }
+*/
